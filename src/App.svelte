6128462bc3e21e074c0b984a2046e8642e905b5a
Brian Sayler <sayler.b@gmail.com>
2020-02-23 22:14:33 -0600

    example of an async test

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,11 @@
 <script>
   export let name;
+
+  let count = 0;
+
+  function increment() {
+    count = count + 1;
+  }
 </script>
 
 <style>
@@ -31,4 +37,6 @@
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
+  <h1 data-testid="count">{count}</h1>
+  <button on:click={increment}>+</button>
 </main>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render } from "@testing-library/svelte";
+import { render, fireEvent } from "@testing-library/svelte";
 import App from "./App.svelte";
 
 test("app says hello", () => {
   const { getByText } = render(App, { name: "World" });
 
   expect(getByText("Hello World!")).toBeInTheDocument();
+});
+
+test("count is initially 0", () => {
+  const { getByTestId } = render(App, { name: "World" });
+
+  expect(getByTestId("count")).toHaveTextContent("0");
+});
+
+// Note: This is as an async test since we are using `fireEvent`
+test("count increments when button is clicked", async () => {
+  const { getByText, getByTestId } = render(App, { name: "World" });
+
+  const button = getByText("+");
+
+  await fireEvent.click(button);
+
+  const count = getByTestId("count");
+
+  expect(count).toHaveTextContent("1");
 });
